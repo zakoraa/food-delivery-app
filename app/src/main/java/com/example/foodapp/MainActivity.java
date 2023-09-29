2b48070fa217fb8469224feef35742a95680a51a
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-29 22:11:23 +0700

    style: add get login page view

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btnToLoginActivity;
+
+    private void initView(){
+        btnToLoginActivity = findViewById(R.id.btn_get_started);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        onBtnToLoginListener();
+    }
+
+    private void onBtnToLoginListener(){
+        btnToLoginActivity.setOnClickListener(v->{
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
     }
 }
