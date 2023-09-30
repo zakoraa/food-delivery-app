db32134ce0920321f8b423e3e62697a273b3154b
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-30 17:01:31 +0700

    style: add bottom sheet animation

--- app/src/main/java/com/example/foodapp/LoginActivity.java
+++ app/src/main/java/com/example/foodapp/LoginActivity.java
@@ -2,13 +2,48 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private RelativeLayout bottomSheet;
+
+    private void initView(){
+        bottomSheet = findViewById(R.id.rl_btn_to_show_sign_up);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+
+        bottomSheet.setOnClickListener(v->{
+            showBottomSheet();
+        });
+    }
+
+    private void showBottomSheet(){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetsignup);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.BottomSheetAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
