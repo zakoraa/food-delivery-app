1ee9b4d32411c938c77dc08f4d2d4a14025e6cf9
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-03 16:47:21 +0700

    feat: add feature to product detail page

--- app/src/main/java/com/example/foodapp/DetailProductActivity.java
+++ app/src/main/java/com/example/foodapp/DetailProductActivity.java
@@ -3,12 +3,24 @@ package com.example.foodapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DetailProductActivity extends AppCompatActivity {
+    private ImageView ivBackToHomePage;
+
+    private void initView(){
+        ivBackToHomePage = findViewById(R.id.iv_back_to_home_page);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_product);
+
+        initView();
+
+        ivBackToHomePage.setOnClickListener(v->{
+            super.onBackPressed();
+        });
     }
 }
--- app/src/main/java/com/example/foodapp/HomeActivity.java
+++ app/src/main/java/com/example/foodapp/HomeActivity.java
@@ -2,13 +2,27 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private LinearLayout lyDetailProduct;
+
+    private void initView(){
+        lyDetailProduct = findViewById(R.id.ly_detail_product);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initView();
+
+        lyDetailProduct.setOnClickListener(v->{
+            startActivity(new Intent(this, DetailProductActivity.class));
+        });
     }
 }
